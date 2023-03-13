9142ecc930349ac45c9b5c5ee7e9b20e9cf08d12
Rulbed <pathrulbed@gmail.com>
2023-03-13 15:55:32 +0700

    receive input number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EXAM1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input x-coordinate of the center of rectangle 1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please input y-coordinate of the center of rectangle 1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please input width of rectangle 1: ");
+            int w1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please input height of rectangle 1: ");
+            int h1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please input x-coordinate of the center of rectangle 1: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please input y-coordinate of the center of rectangle 1: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please input width of rectangle 1: ");
+            int w2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please input height of rectangle 1: ");
+            int h2 = Convert.ToInt32(Console.ReadLine());
+
+            
+
+        }
+    }
+}
